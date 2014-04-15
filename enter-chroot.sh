d35df714c6cdf1b0f518c236825115c61e261219
Samuel Raynor <samuel@samuelraynor.com>
2014-04-15 20:57:24 +0000

    Removed old scripts

--- enter-chroot.sh
+++
@@ -1,6 +0,0 @@
-chroot "$LFS" /tools/bin/env -i \
-	HOME=/root \
-	TERM="$TERM" \
-	PS1='[\u : \w]\$ ' \
-	PATH=/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin:/tools/bin \
-	/tools/bin/bash --login +h
